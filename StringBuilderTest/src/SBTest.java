fc4709791e9f36354fb92cc7f6955bed2696f227
yuzengyi <1159521080@qq.com>
2023-03-28 19:30:53 +0800

    stringbuilder

--- StringBuilderTest/src/SBTest.java
+++ StringBuilderTest/src/SBTest.java
@@ -2,9 +2,11 @@ public class SBTest{
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         //System.out.println(sb);//print is shuxingzhi
-        sb.append(1);
-        sb.append(2.3);
-        System.out.println(sb);
+        // sb.append(1);
+        // sb.append(2.3);
+        // System.out.println(sb);
+        
+        sb.reverse();
 
     }
 
